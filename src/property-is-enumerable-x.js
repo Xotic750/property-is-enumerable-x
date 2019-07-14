717692df2e4ad8c44a27f0bfe5c95369d0641d9b
Graham Fairweather <xotic750@gmail.com>
2019-07-14 02:39:06 +0200

    Terraformed
    Automated build

--- src/property-is-enumerable-x.js
+++ src/property-is-enumerable-x.js
@@ -1,30 +1,29 @@
 /**
  * @file Indicates whether the specified property is enumerable.
- * @version 1.1.0
- * @author Xotic750 <Xotic750@gmail.com>
- * @copyright  Xotic750
+ * @version 1.1.0.
+ * @author Xotic750 <Xotic750@gmail.com>.
+ * @copyright  Xotic750.
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
- * @module property-is-enumerable-x
+ * @module Property-is-enumerable-x.
  */
 
-'use strict';
+const toPropertyKey = require('to-property-key-x');
+const toObject = require('to-object-x');
 
-var toPropertyKey = require('to-property-key-x');
-var toObject = require('to-object-x');
-var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+const propIsEnumerable = Object.prototype.propertyIsEnumerable;
 
 /**
  * This method returns a Boolean indicating whether the specified property is
  * enumerable. Does not attempt to fix bugs in IE<9 or old Opera, otherwise it
  * does ES6ify the method.
  *
- * @param {!Object} object - The object on which to test the property.
+ * @param {!object} object - The object on which to test the property.
  * @param {string|Symbol} property - The name of the property to test.
  * @throws {TypeError} If target is null or undefined.
  * @returns {boolean} A Boolean indicating whether the specified property is
  *  enumerable.
  * @example
- * var propertyIsEnumerable = require('property-is-enumerable-x');
+ * var propertyIsEnumerable = require('property-is-enumerable-x');.
  *
  * var o = {};
  * var a = [];
