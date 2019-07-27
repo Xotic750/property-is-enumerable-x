76c6e78c7a70367b6fc26c4ff978e1b9a6f82d6d
Graham Fairweather <xotic750@gmail.com>
2019-07-27 16:22:25 +0200

    :bookmark: v2.0.18
    Auto build

--- dist/property-is-enumerable-x.js
+++ dist/property-is-enumerable-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-27T01:07:57.095Z",
+  "date": "2019-07-27T14:22:20.045Z",
   "describe": "",
   "description": "Indicates whether the specified property is enumerable.",
   "file": "property-is-enumerable-x.js",
   "hash": "8b678e563320fdcab4a5",
   "license": "MIT",
-  "version": "2.0.17"
+  "version": "2.0.18"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
