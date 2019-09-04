95558b14c4e72c50c5b2702669436f7766bf71de
Graham Fairweather <xotic750@gmail.com>
2019-09-05 01:20:44 +0200

    Maintenance

--- dist/property-is-enumerable-x.js
+++ dist/property-is-enumerable-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-30T22:02:53.346Z",
+  "date": "2019-09-04T23:20:38.136Z",
   "describe": "",
   "description": "Indicates whether the specified property is enumerable.",
   "file": "property-is-enumerable-x.js",
